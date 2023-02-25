dd8723bf9950318654509b1382f9c7c5cb78edb2
ridhopujiono <ridho@smktjp.sch.id>
2023-02-26 00:41:53 +0700

    done all

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const app = express();
+const http = require('http');
+const WebSocket = require('ws');
+const port = 6969;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({server});
 
-// Mengakses file Bootstrap dari folder 'node_modules'
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-
-// Menampilkan file index.html
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach( function each(client) {
+        if(client !== ws && client.readyState === WebSocket.OPEN){
+            client.send(JSON.stringify(data));
+        }
+    })
+  });
 });
-
-// Menjalankan server
-app.listen(3000, () => console.log('Server berjalan di port 3000!'));
+server.listen(port, function(){
+    console.log('Server is listening on port '+ port);
+});
